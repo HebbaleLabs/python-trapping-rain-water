fc50f2fb327346d50bc08cbb0ba343b0b53097b2
Harsha Bhargav <harsha.bhargav@hebbalelabs.com>
2019-08-06 23:46:43 +0530

    Initial Commit

---
+++ tests/test_trap_rain_water.py
@@ -0,0 +1,22 @@
+import unittest
+from parameterized import parameterized
+from trap_rain_water import do_trap_rain_water
+
+
+class TrapRainWaterTest(unittest.TestCase):
+
+    @parameterized.expand([
+        ([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6),
+        ([3, 0, 0, 2, 0, 4], 10),
+        ([2, 0, 2], 2),
+        ([1, 4, 2, 5, 0, 6, 2, 3, 4], 10),
+        ([0, 4, 5, 1, 0, 0], 0),
+        ([7, 0, 4, 2, 5, 0, 6, 4, 0, 5], 25),
+        ([1, 0, 2, 2, 4, 0, 1, 5, 2, 1, 6, 1], 15),
+        ([4, 2, 3, 5, 2, 3, 4, 5], 9),
+    ])
+    def test_trap_rain_water(self, input_value, expected_result):
+        self.longMessage = True
+        actual = do_trap_rain_water(input_value)
+        message = 'For input {0}, expected result = {1}, and actual value = {2}'.format(input_value, expected_result, actual)
+        self.assertEqual(expected_result, actual, message)
---
+++ trap_rain_water.py
@@ -0,0 +1,19 @@
+def do_trap_rain_water(input_value):
+    #
+    # IMPORTANT:
+    # 1. Write your solution in this function
+    # 2. Do not make changes to the function signature
+    # 3. Use the 'pip-install' command in the IDE, to install any requirements. This takes a few seconds.
+    # 4. Use the 'run' command in the IDE, to run the main method. You can also invoke the main method from the terminal.
+    # 5. Use the 'run-tests' command in the IDE, to run unit tests. The 'pip-install' command must be run before this.
+    return 6
+
+
+def main():
+    input_value = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+    trapped_rain_water = do_trap_rain_water(input_value)
+    print('Trapped Rain Water : {0}\n'.format(trapped_rain_water))
+
+
+if __name__ == '__main__':
+    main()
